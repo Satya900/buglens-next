ae7ef4edfe621a5c9bdaf8bdffc25d9b8ca6aab8
Satyabrata Mohanty <mohantysatyabrata900@gmail.com>
2026-04-21 18:58:10 +0530

    feat: add robots.txt configuration for search engine indexing control

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,17 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      disallow: [
+        "/*/opengraph-image/",
+        "/opengraph-image/",
+        "/*/twitter-image/",
+        "/twitter-image/",
+        "/*/icon/",
+        "/icon/",
+        "/*/apple-icon/",
+        "/apple-icon/",
+        "/manifest/",
+      ],
     },
     host: siteConfig.url,
     sitemap: getAbsoluteUrl("/sitemap.xml"),
